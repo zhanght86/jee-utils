1e8379a52d89cba37583e6aa1fd83be7b7868eac
ck <svsechen@163.com>
2017-03-20 18:09:06 +0800

    使用base64 将二进制文件转成文本文件,再将文本文件转回来

---
+++ src/test/java/com/ckjava/utils/TestFileUtils.java
@@ -0,0 +1,64 @@
+package com.ckjava.utils;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class TestFileUtils {
+	public static void main(String[] args) {
+		enCodeFile();
+		
+		deCodeFile();
+	}
+
+	private static void deCodeFile() {
+		String data = "D:\\BaiduYunDownload\\dagger.txt";
+		
+		String temp = "";
+		try {
+			BufferedReader is = new BufferedReader(new FileReader(new File(data)));
+			BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(new File("D:\\BaiduYunDownload\\dagger.zs.jar")));
+			while ((temp = is.readLine()) != null) {
+				byte[] str = Base64.decodeBase64(temp.getBytes());
+				os.write(str);
+			}
+			is.close();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+	}
+
+	public static void enCodeFile() {
+		String data = "D:\\BaiduYunDownload\\dagger.jar";
+		
+		int temp = 0;
+		byte[] b = new byte[102400];
+		try {
+			InputStream is = new FileInputStream(new File(data));
+			BufferedWriter os = new BufferedWriter(new FileWriter(new File("D:\\BaiduYunDownload\\dagger.txt")));
+			while ((temp = is.read(b, 0, b.length)) != -1) {
+				byte[] realdata = new byte[temp];
+				System.arraycopy(b, 0, realdata, 0, temp);
+				byte[] str = Base64.encodeBase64(realdata);
+				os.write(new String(str));
+				os.newLine();
+			}
+			is.close();
+			os.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+	}
+}
