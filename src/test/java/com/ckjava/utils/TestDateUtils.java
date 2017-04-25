82f89ab43d7017e73ecec5e78b7953c54d655b48
svsechen <svsechen@163.com>
2017-04-25 21:13:00 +0800

    完善DateUtils

--- src/test/java/com/ckjava/utils/TestDateUtils.java
+++ src/test/java/com/ckjava/utils/TestDateUtils.java
@@ -1,13 +1,12 @@
 package com.ckjava.utils;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TestDateUtils {
 	
 	@Test
 	public void TestGetCurrentTimeStamp() {
-		String datetime = DateUtils.getCurrentTimeStamp();
-		Assert.assertNotNull(datetime);
 	}
+	
+	
 }
